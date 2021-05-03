fd6693df8660880dabf53986af99ffc6c85fb4f3
Rohan Bansal <rohanarunbansal@gmail.com>
2021-05-03 10:18:49 -0400

    Begin chapter 1 exercises

---
+++ 1-intro/README.md
@@ -0,0 +1,13 @@
+# Chapter 1 - A Tutorial Introduction
+
+## 1.1 - Getting Started
+
+### Exercise 1.1
+
+See [hello_world.c](hello_world.c).
+
+### Exercise 1.2
+
+Unknown escape sequence.
+
+## 1.2 - Variables and Arithmetic Expressions
---
+++ 1-intro/fahr_celsius_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int fahr, celsius;
+
+  int lower = 0;
+  int upper = 300;
+  int step = 20;
+ 
+  fahr = lower;
+  while (fahr <= upper)
+  {
+    celsius = (5 * (fahr - 32)) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr += 20;
+  }
+}
